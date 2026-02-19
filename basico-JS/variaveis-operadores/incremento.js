1554875733c2a03e66f103e5175134700e8588f3
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-19 20:03:29 -0300

    variaveis e operadores

---
+++ basico-JS/variaveis-operadores/incremento.js
@@ -0,0 +1,14 @@
+
+
+
+let num = 1;
+
+console.log(++num);
+console.log(++num);
+console.log(--num);
+
+console.log(`__________________________________________`);
+console.log(num += 2);
+console.log(num *= 2);
+
+
---
+++ basico-JS/variaveis-operadores/operadores.js
@@ -0,0 +1,15 @@
+const v1 = 10;
+const v2 = 20;
+const dividir = 10 % 11
+
+console.log(v1 + v2);
+
+console.log((v1 + v1) * v2);
+
+console.log(100 - 100);
+
+console.log(dividir);
+
+console.log(5**3);
+
+
