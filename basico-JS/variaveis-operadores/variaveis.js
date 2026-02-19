9a4bc23a106b69f37c23e9b17a01fda8dba4f219
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-19 19:47:56 -0300

    voltando aos estudos de js

---
+++ basico-JS/variaveis-operadores/variaveis.js
@@ -0,0 +1,17 @@
+// console.log(`oiiiii`)
+const idade = 20;
+
+
+const comp = `oiii`
+let userName;
+
+console.log(comp);
+userName = `Fagner`
+console.log(`-----------------------------------------`);
+console.log(userName);
+
+console.log(typeof userName)
+
+console.log(`---------------------------------------------`);
+console.log(`${comp}, meu nome é ${userName} e tenho ${idade} anos`);
+
