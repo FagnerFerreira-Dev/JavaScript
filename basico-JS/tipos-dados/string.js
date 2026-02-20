6c071c1b76ab1dce2dd00df34d8c964c7607eb6a
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-20 18:14:21 -0300

    primeiras funcoes com number

---
+++ basico-JS/tipos-dados/string.js
@@ -0,0 +1,8 @@
+const num1 = 100202020;
+const num2 = 12.543;
+
+console.log(num2.toFixed(1));
+console.log(Number.isInteger(num1));
+console.log(Number.isInteger(num2));
+console.log(Number.isNaN(num1));
+
