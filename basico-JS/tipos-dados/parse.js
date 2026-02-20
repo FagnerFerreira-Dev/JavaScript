d00b841e205a72952cf90d9a1dda289bbbec4065
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-19 21:14:12 -0300

    entendendo number e string

---
+++ basico-JS/tipos-dados/parse.js
@@ -0,0 +1,12 @@
+const nume1 = parseInt(prompt(`digite um número`));
+const nume2 = Number(prompt(`digite um número`));
+console.log(typeof nume2);
+
+
+alert(`a sama dos dois números é: ${nume1 + nume2}`)
+
+let num3 = 12030203203;
+console.log(typeof num3);
+num3 = num3.toString();
+console.log( typeof num3);
+
