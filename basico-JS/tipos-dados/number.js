64e52ce7ea36ac32a9560d76c9b76735acb74919
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-20 18:23:58 -0300

    entendendo objeto Math

---
+++ basico-JS/tipos-dados/number.js
@@ -0,0 +1,19 @@
+const num1 = 100202020;
+const num2 = 12.543;
+
+console.log(num2.toFixed(1));
+console.log(Number.isInteger(num1));
+console.log(Number.isInteger(num2));
+console.log(Number.isNaN(num1));
+
+console.log(Math.floor(num2));
+console.log(Math.ceil(num2));
+console.log(Math.round(num2));
+
+console.log(Math.min(1,2,3,5));
+console.log(Math.max(56,63,3,2,1));
+
+console.log(Math.random().toFixed(1) * 20);
+
+
+
--- basico-JS/tipos-dados/string.js
+++
@@ -1,8 +0,0 @@
-const num1 = 100202020;
-const num2 = 12.543;
-
-console.log(num2.toFixed(1));
-console.log(Number.isInteger(num1));
-console.log(Number.isInteger(num2));
-console.log(Number.isNaN(num1));
-
