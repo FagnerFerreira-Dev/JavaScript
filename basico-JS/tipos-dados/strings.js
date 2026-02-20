43e1679f5cfb5f43db097a018f34bf45da35969e
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-19 21:35:59 -0300

    entendedo mais strings

---
+++ basico-JS/tipos-dados/strings.js
@@ -0,0 +1,19 @@
+let nome = `Fagner Ferreira Bagundes`
+console.log(nome);
+
+let sla = `bola bolinha bola bolona bola`
+console.log(nome[7]);
+
+console.log(nome.charAt(16));
+
+nome = nome.concat(` tem 20 anos de idade`)
+console.log(nome);
+
+console.log(nome.indexOf(`Bagundes`));
+
+console.log(sla.indexOf(`bola`, 7));
+
+console.log(sla.lastIndexOf(`bola`));
+
+console.log(sla.lastIndexOf(`bola`, 20));
+
