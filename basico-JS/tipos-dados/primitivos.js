4f84402a0f397fc2a7a666468c80bcd00ac3d836
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-19 21:00:24 -0300

    entendendo dados primitivos

---
+++ basico-JS/tipos-dados/primitivos.js
@@ -0,0 +1,20 @@
+const num = 100;
+const nome = `Fagner`
+let fag;
+const verdadeiro = true;
+const falso = false;
+const nulo = null;
+
+
+console.log(num, typeof num);
+
+console.log(nome, typeof nome);
+console.log(fag, typeof fag);
+console.log(verdadeiro, typeof verdadeiro);
+
+console.log(falso, typeof falso);
+console.log(nulo, typeof nulo);
+
+
+
+
