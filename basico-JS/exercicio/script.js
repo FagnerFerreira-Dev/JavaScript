35dafd7ee5b61abd3049d6a1295d6e0a0764d2d7
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-21 17:55:25 -0300

    entendendo addEventListener

--- basico-JS/exercicio/script.js
+++ basico-JS/exercicio/script.js
@@ -1,3 +1,15 @@
 const btn = document.querySelector(`input`);
 const body =document.querySelector(`body`);
+const h1 = document.querySelector(`h1`)
 
+
+btn.addEventListener(`click`, (e)=>{
+body.innerHTML+=`Cricado`;
+console.log(e);
+
+})
+
+function troca() {
+    h1.innerHTML = `trocou o textos`
+}
+h1.addEventListener(`click`, troca)
