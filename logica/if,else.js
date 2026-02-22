40f5f5ef9a1b972a2af8977670929ea62c4a2bd7
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-22 20:18:48 -0300

    entendendo if e else

---
+++ logica/if,else.js
@@ -0,0 +1,22 @@
+let nome = undefined
+
+
+if(nome){
+    console.log(nome);
+    
+} else{
+    console.log(`não existe nome`);
+    
+}
+
+nome =`Fagner`
+
+
+
+if(nome){
+    console.log(nome);
+    
+} else{
+    console.log(`não existe nome`);
+    
+}
