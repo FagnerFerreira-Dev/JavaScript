2fe35275fe639274761068eeff7fb556a8f2c228
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-24 20:13:26 -0300

    entendendo destructuring-assignnment

---
+++ logica/destructuring-assignmnet.js
@@ -0,0 +1,23 @@
+let a = `A`;
+let b = `B`;
+let c = `C`;
+
+console.log(a,b,c);
+
+[a,b,c] = [b,c,a]
+
+
+console.log(a,b,c);
+
+
+let n1 = 0;
+let n2 = 0;
+let n3 = 0;
+
+const numeros = [10,20,30];
+
+console.log(numeros);
+
+[n1,n2,n3] = numeros;
+
+console.log(n1, n2, n3);
