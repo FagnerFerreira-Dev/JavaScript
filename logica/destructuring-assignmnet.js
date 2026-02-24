522fe5ed0b89beb4901c053eb52ba4632bc9d277
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-24 20:18:33 -0300

    entendendo rest operator

--- logica/destructuring-assignmnet.js
+++ logica/destructuring-assignmnet.js
@@ -21,3 +21,12 @@ console.log(numeros);
 [n1,n2,n3] = numeros;
 
 console.log(n1, n2, n3);
+
+
+
+const vNums = [10,20,30,40,50,10,20,30,40,50]
+
+
+let =[ nu1,nu2,nu3,nu4,nu5, ...resto] = vNums
+
+console.log(nu1,nu2,nu3,nu4,nu5,resto);
