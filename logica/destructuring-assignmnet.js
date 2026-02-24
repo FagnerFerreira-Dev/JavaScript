b20450a1668e116500672cedb0feea3ce5bbed42
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-24 20:41:00 -0300

    objetos dentro de objetos

--- logica/destructuring-assignmnet.js
+++ logica/destructuring-assignmnet.js
@@ -30,3 +30,40 @@ const vNums = [10,20,30,40,50,10,20,30,40,50]
 let =[ nu1,nu2,nu3,nu4,nu5, ...resto] = vNums
 
 console.log(nu1,nu2,nu3,nu4,nu5,resto);
+
+
+
+// atriuição via desestruturação com objetos:
+
+const pessoa = { 
+    nome: `Fagner`,
+    sobrenome: `Ferreira`,
+    idade: 20
+}
+
+
+const {nome, sobrenome, idade} = pessoa;
+console.log(nome, sobrenome, idade);
+
+const {nome: id, sobrenome: id2, idade: old} = pessoa;
+console.log(id,id2,old);
+
+
+// objetos dentro de objetos:
+
+const casa = {
+    cor: `green`,
+    tipo: `madeira`,
+    endereco: {
+        city: `macapá`,
+        bairro: `zerão`
+    }
+}
+
+
+// const {cor, tipo, endereco: {city, bairro}} = casa
+// console.log(cor, tipo, city, bairro);
+
+const{endereco: {city: cidade, bairro: rua}, cor: cor1, tipo: tipo1} = casa
+
+console.log(cidade, rua, tipo1, cor1, cidade);
