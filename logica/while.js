abc1659e22804fbb5eccf18bdebf72abc102d390
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-25 20:27:18 -0300

    entendendo break e continue

--- logica/while.js
+++ logica/while.js
@@ -1,18 +1,47 @@
 
 
-function gRandom(v){
-    return Math.ceil((Math.random() * v))
-} 
+// function gRandom(v){
+//     return Math.ceil((Math.random() * v))
+// } 
 
-let random = gRandom(5)
+// let random = gRandom(5)
 
 
-while (random !== 5) {
-    console.log(random);
-    random = gRandom(5)
+// while (random !== 5) {
+//     console.log(random);
+//     random = gRandom(5)
+// }
+
+// do {
+//     console.log(random);
+//     random = gRandom(5)
+// } while (random !== 5);
+
+
+
+
+
+let lista = [1,2,3,4,5,6,7,8,9,10];
+
+// for (let v of lista){
+//     if ( v % 2 === 1) {
+//         continue
+//     }
+//     console.log(v);
+    
+// }
+
+let cor = `roxo`
+
+switch(cor){
+    case `vermelho`:
+        console.log(`vermelho`);
+        break;
+    case `verde`: 
+        console.log(`verde`);
+        break;
+    case `roxo`: 
+        console.log(`roxo`);
+        break;
+
 }
-
-do {
-    console.log(random);
-    random = gRandom(5)
-} while (random !== 5);
