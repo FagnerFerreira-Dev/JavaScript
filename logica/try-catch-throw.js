84e11f595780ea2e9e0e0893f0794d93dfe2c978
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-02-26 18:14:31 -0300

    entendendo throw n try

--- logica/try-catch-throw.js
+++ logica/try-catch-throw.js
@@ -9,11 +9,39 @@
 // }
 
 
-try{
-    console.log(nome);
-}catch(e){
-    console.log(e.name);
-} finally{
-    console.log(`operação finalilzada`);
+// try{
+//     console.log(nome);
+// }catch(e){
+//     console.log(e.name);
+// } finally{
+//     console.log(`operação finalilzada`);
     
+// }
+let erro =new Error("");
+;
+
+
+
+function dividir(a, b) {
+    try {
+        if (b === 0) {throw new Error("Divisão por zero")}
+        if (a,b) {
+            return a / b;
+        } 
+
+        console.log(g);
+        
+        
+    } catch (error) {
+        console.log(error.message);
+        console.log(error.name);
+        console.log(error);
+        
+    } finally{
+        console.log(`operação finalizada`);
+        
+    }
 }
+
+
+dividir(erro)
