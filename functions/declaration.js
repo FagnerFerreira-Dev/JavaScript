56584ef3143698086e95e504c99a488e51a279ff
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-03-01 21:10:53 -0300

    entendedo maneiras de chamar funcoes

---
+++ functions/declaration.js
@@ -0,0 +1,21 @@
+dentro de objetos:
+
+const cachorro = {
+    nome: `billy`,
+    idade: 5,
+    latir(){
+        console.log(`${this.nome}: au au`);
+    }
+}
+
+console.log(cachorro);
+cachorro.latir()
+
+function algo(){
+    for (let arg of arguments )[
+        console.log(arguments[arg])
+    ]
+    
+}
+
+algo(2,3,5,6,7,8,5,4,3,3)
