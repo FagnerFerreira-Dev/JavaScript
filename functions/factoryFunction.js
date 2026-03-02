18d38732f36fe8d7dd5fc246813b5df700c017b4
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-03-02 17:52:35 -0300

    praticando factoryFunctions

--- functions/factoryFunction.js
+++ functions/factoryFunction.js
@@ -1,17 +1,11 @@
-function criaPessoa(nome, sobrenome) {
-    return {
-        nome,
-        sobrenome
-    }
-}
+let criaPessoa = (nome, sobrenome) => ({nome,sobrenome})
+
 
 
 let p1 = criaPessoa(`Fagner`, `Ferreira`)
 console.log(p1);
 
-function criaAnimal(especie, nome, faz) {
-    return { nome, especie, ativar(){console.log(`esse animal faz ${faz}`); }}
-}
+let criaAnimal = (especie, nome, faz) => ({nome, especie, ativar(){console.log(`esse animal faz ${faz}`)}})
 
 
 
@@ -25,3 +19,39 @@ console.log(Cachorro, Gato);
 
 Cachorro.ativar()
 Gato.ativar()
+
+
+console.log(`--------------------------------------------------------------------------------------------------------`);
+
+function contador(encrementor) {
+    let contador = 0;
+
+    return {
+        incre(){
+            contador += encrementor
+        },
+
+        ver(){
+            console.log(contador);
+            
+        }
+    }
+}
+
+
+let encrementaDe2 = contador(2)
+
+encrementaDe2.incre()
+encrementaDe2.incre()
+encrementaDe2.incre()
+encrementaDe2.incre()
+encrementaDe2.incre()
+encrementaDe2.ver()
+
+
+let encrementaDe3 = contador(3)
+encrementaDe3.incre()
+encrementaDe3.incre()
+encrementaDe3.incre()
+encrementaDe3.incre()
+encrementaDe3.ver()
