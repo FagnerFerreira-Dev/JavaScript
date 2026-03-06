8626951b3a49e02886bdf982d02da145c5b27180
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-03-06 17:54:35 -0300

    entendendo findIndex e find

---
+++ arrays/find.js
@@ -0,0 +1,14 @@
+let lista =[`Fagner`, `Nalda`, `Wellington`,`dudu`, `gugu`]
+
+let nomeM = lista.find(valor => valor.length < 5);
+
+let nomeMIndice = lista.findIndex(valor => valor.length < 5);
+
+console.log(nomeMIndice);
+
+console.log(nomeM);
+
+
+
+
+
