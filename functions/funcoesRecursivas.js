a12472494a48816640032c86443112b12e23eac9
Fagner Ferreira Bagundes <fagnerferreira.dev@gmail.com>
2026-03-07 17:44:53 -0300

    .

--- functions/funcoesRecursivas.js
+++ functions/funcoesRecursivas.js
@@ -1,19 +1,8 @@
 function fatorial(n) {
-
-    if (n === 0){
-        // console.log(n);
-        return 1;
-    }
-    
-    
+    if (n === 0) return 1;
     return n * fatorial(n-1) 
-    
-
 }
 
-
-// fatorial(5)
-console.log(`----------------------`);
 
 console.log(fatorial(5));
 
